b3c812c90cf41b6d9f30d82cb291ef7cf100ee1c
jhickey <jameshickey@me.com>
2016-12-22 21:05:56 -0500

    some tests

--- test/index.js
+++ test/index.js
@@ -9,7 +9,11 @@ const flatMap = Immutable.Map({a: 1, b: 2, c: 3});
 
 describe('modash', () => {
 
-  it('must work on a plain js object', () => {
-      expect(plainObj::_.omit('a')).to.equal({b: 2, c: 3});
+  it('should work on a plain js object', () => {
+      expect(plainObj::_.omit('a')).to.deep.equal({b: 2, c: 3});
+  });
+  it('should work on an immutable object', () => {
+    const ret = flatMap::_.omit('a');
+    expect(ret.toJS()).to.deep.equal({b: 2, c: 3});
   });
 });
